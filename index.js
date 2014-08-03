cf6f5f38dc9ea6a33b7d1de677420a4a7d73fe37
jesse keane <me@jessekeane.me>
2014-08-03 15:20:17 -0700

    lookup face

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var coolFace = require('cool-ascii-faces')
 
+var lookup = /^\!face \d+$/
+
 cool.help = '!face - print a cool ascii face'
 
 module.exports = cool
@@ -9,5 +11,8 @@ function cool(ziggy) {
 
   function parseMessage(user, channel, message) {
     if(message === '!face') ziggy.say(channel, coolFace())
+    if(lookup.test(message)) {
+      ziggy.say(channel, coolFace.faces[+message.slice(6)])
+    }
   }
 }
